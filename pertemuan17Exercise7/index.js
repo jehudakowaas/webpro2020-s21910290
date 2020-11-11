4dd82583de0ba536c17a05270b602ced1465f4b7
jehudakowaas <s21910290@student.unklab.ac.id>
2020-11-11 22:20:51 +0800

    Exercise7

---
+++ pertemuan17Exercise7/index.js
@@ -0,0 +1,49 @@
+// Variable
+
+// //let and const
+// let _umur = 25;
+// _umur = 26;
+// let nilai = 95.5;
+// let sudahMenikah = true;
+// let $nama = "John Doe";
+// console.log(_umur);
+// console.log($nama);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir - 2002;
+// console.log(tahunLahir);
+
+// Operator
+
+// Aritmathic Operator (+,-,*,/)
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1 + angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";   
+// console.log("Nama saya" + namaDepan + " " + namaBelakang + ". Umur saya adalah" + umur + "tahun");
+
+// Operator Perbandingan
+
+// let angka = 5;
+// let hasil = angka <= 0 ? "Negatif" : "Positif"
+// console.log(hasil);
+
+// Operator Logika
+// let kondisi1 = 10;
+// let kondisi2 = 11;
+// console.log( kondisi1 || kondisi2);
+
+// let angka = prompt("Masukkan angka :");
+// console.log("angka yang dimasukkan adalah" + angka);
+
+
+// Exercise 7
+let angka1 = prompt ("Masukkan Berat :");
+let angka2 = prompt ("Masukkan Tinggi :");
+console.log("BMI anda adalah" + angka1 / (angka2 * angka2));
